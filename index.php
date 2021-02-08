c5b7d27fe00032417e535c3c309054cfc4ca4126
Karl Heitmann <khf1988@gmail.com>
2021-02-07 22:42:28 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name: Restaurant valorizador
+ * Description: Un plugin para valorizar los restaurants.
+ * Version: 1.0
+ * Author: Karl Heitmann
+ * Author URI: https://karlheitmann.github.io
+ * Text domain: restaurant-valorizador
+ */
+
+// Setup
+define( 'RESTAURANT_VALORIZADOR_PLUGIN_URL', __FILE__);
+
