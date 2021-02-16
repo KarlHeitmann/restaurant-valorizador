5766fe11dba4de9040cdb71ada06471656947d6d
Karl Heitmann <khf1988@gmail.com>
2021-02-16 02:26:57 -0300

    agregado dist files

--- blocks/app/restaurant-valorizar-block/item.js
+++ blocks/app/restaurant-valorizar-block/item.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const icono_star_old = <svg width="24" height="28" viewbox="0 0 24 28" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" clip-rule="evenodd" d="M24 27.7846L12 7L0 27.7846H24ZM17.2861 23.8903L11.957 14.6599L6.62776 23.8903H17.2861Z" fill="#3C6F8D"/>
   <path d="M7 12L5.5 10L12 1L18 10L16.5 11.5L12 4.5L7 12Z" fill="#3C6F8D" stroke="black"/>
